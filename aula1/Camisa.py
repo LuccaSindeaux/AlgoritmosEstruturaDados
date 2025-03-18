8ffbf65f56ca61b948a2d6af25a25ac3e52063ac
Lucca Sindeaux <luccasindeaux@gmail.com>
2025-03-17 21:41:41 -0300

    iniciando aulas

---
+++ aula1/Camisa.py
@@ -0,0 +1,14 @@
+class Camsisa:
+    def __init__(self, tamanho,cor):
+        self.tamanho = tamanho
+        self.cor = cor
+        self.preco = 0
+
+
+'''Posso declarar uma variável como deafult'''
+
+# class Camsisa:
+#     def __init__(self, tamanho="M",cor ='Amarela'):
+#         self.tamanho = tamanho
+#         self.cor = cor
+#         self.preco = 0
