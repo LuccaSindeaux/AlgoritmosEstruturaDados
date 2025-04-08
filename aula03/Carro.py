6a4b8f2f20272d6b975e943f359f93eb55a95c65
Lucca Sindeaux <luccasindeaux@gmail.com>
2025-04-07 21:11:42 -0300

    Adicionando Carro.py com suas geometrias

---
+++ aula03/Carro.py
@@ -0,0 +1,9 @@
+from Veiculos import Veiculos
+
+class Carro( Veiculos ):
+    def __init__(self, modelo=None, ano=2025, qtd = 4):
+        super().__init__(modelo, ano)
+        self.portas = qtd
+
+    def __str__(self):
+        return super().__str__() + "\nPortas: " + str( self.portas)
